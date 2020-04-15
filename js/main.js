1e234a6decc05abb7e0c3c8add1523fdc27a38fc
kkashyn <61836023+kkashyn@users.noreply.github.com>
2020-04-15 11:21:18 +0300

    small fixes

--- js/main.js
+++ js/main.js
@@ -1,44 +1,46 @@
 function createCarousel(slidesCount = 5) {
-  let outSlides = '';
+  let outSlides = "";
   let slideItems = '<li class="slides__item active"><a href="#"></a></li>';
-  let styleItems = '';
-  let indicatorItemOut = '<span class="indicators__item active" data-slide-to="0"></span>';
+  let styleItems = "";
+  let indicatorItemOut =
+    '<span class="indicators__item active" data-slide-to="0"></span>';
 
   for (let i = 1; i < slidesCount; i++) {
     slideItems += '<li class="slides__item"><a href="#"></a></li>';
     indicatorItemOut += `<span class="indicators__item" data-slide-to="${i}"></span>`;
   }
-  document.getElementById('carousel').innerHTML = outSlides + document.getElementById('carousel').innerHTML;
+  document.getElementById("carousel").innerHTML =
+    outSlides + document.getElementById("carousel").innerHTML;
 }
 
 createCarousel(5);
 
-const SPACE = ' ';
-const LEFT_ARROW = 'ArrowLeft';
-const RIGHT_ARROW = 'ArrowRight';
+const SPACE = " ";
+const LEFT_ARROW = "ArrowLeft";
+const RIGHT_ARROW = "ArrowRight";
 
-let slides = document.querySelectorAll('.slides__item');
+let slides = document.querySelectorAll(".slides__item");
 let currentSlide = 0;
-let slideInterval = setInterval(nextSlide, 1500);
+let slideInterval = setInterval(nextSlide, 2000);
 let isPlaying = true;
 
-let pauseButton = document.querySelector('.controls__pause');
-let pauseIcon = document.querySelector('#pause');
-let nextButton = document.querySelector('#next');
-let previousButton = document.querySelector('#previous');
-let controls = document.querySelector('.controls');
+let pauseButton = document.querySelector(".controls__pause");
+let pauseIcon = document.querySelector("#pause");
+let nextButton = document.querySelector("#next");
+let previousButton = document.querySelector("#previous");
+let controls = document.querySelector(".controls");
 
-let indicatorsItems = document.querySelectorAll('.indicators__item');
-let indicators = document.querySelector('.indicators');
-let carousel = document.querySelector('.carousel');
+let indicatorsItems = document.querySelectorAll(".indicators__item");
+let indicators = document.querySelector(".indicators");
+let carousel = document.querySelector(".carousel");
 
 let goToSlide = (n) => {
-  slides[currentSlide].classList.toggle('active');
-  indicatorsItems[currentSlide].classList.toggle('active');
+  slides[currentSlide].classList.toggle("active");
+  indicatorsItems[currentSlide].classList.toggle("active");
   currentSlide = (n + slides.length) % slides.length;
-  slides[currentSlide].classList.toggle('active');
-  indicatorsItems[currentSlide].classList.toggle('active');
-}
+  slides[currentSlide].classList.toggle("active");
+  indicatorsItems[currentSlide].classList.toggle("active");
+};
 
 function nextSlide() {
   goToSlide(currentSlide + 1);
@@ -49,48 +51,48 @@ function previousSlide() {
 }
 
 function pauseSlideShow() {
-  pauseIcon.className = 'fas fa-play';
+  pauseIcon.className = "fas fa-play";
   isPlaying = false;
   clearInterval(slideInterval);
 }
 
 function playSlideShow() {
-  pauseIcon.className = 'fas fa-pause';
+  pauseIcon.className = "fas fa-pause";
   isPlaying = true;
-  slideInterval = setInterval(nextSlide, 1500);
+  slideInterval = setInterval(nextSlide, 2000);
 }
 
 function indicatorsSlide(event) {
   let target = event.target;
 
-  if (target.classList.contains('indicators__item')) {
+  if (target.classList.contains("indicators__item")) {
     pauseSlideShow();
-    goToSlide(+target.getAttribute('data-slide-to'));
+    goToSlide(+target.getAttribute("data-slide-to"));
   }
 }
 
-indicators.addEventListener('click', indicatorsSlide);
+indicators.addEventListener("click", indicatorsSlide);
 
 function toPauseIfPlay() {
   if (isPlaying) pauseSlideShow();
   else playSlideShow();
 }
 
-pauseButton.addEventListener('click', toPauseIfPlay);
+pauseButton.addEventListener("click", toPauseIfPlay);
 
 function toNextSlide() {
   pauseSlideShow();
   nextSlide();
 }
 
-nextButton.addEventListener('click', toNextSlide);
+nextButton.addEventListener("click", toNextSlide);
 
 function toPreviousSlide() {
   pauseSlideShow();
   previousSlide();
 }
 
-previousButton.addEventListener('click', toPreviousSlide);
+previousButton.addEventListener("click", toPreviousSlide);
 
 let pressKeyControl = (e) => {
   if (e.key === LEFT_ARROW) previousSlide();
@@ -98,7 +100,7 @@ let pressKeyControl = (e) => {
   if (e.key === SPACE) toPauseIfPlay();
 };
 
-document.addEventListener('keydown', pressKeyControl);
+document.addEventListener("keydown", pressKeyControl);
 
 let swipeStartX = null;
 let swipeEndX = null;
@@ -111,7 +113,8 @@ let swipeEnd = (e) => {
   swipeEndX = e.changedTouches[0].pageX;
   swipeStartX - swipeEndX < 100 && previousSlide();
   swipeStartX - swipeEndX > -100 && nextSlide();
+  pauseSlideShow();
 };
 
-carousel.addEventListener('touchstart', swipeStart);
-carousel.addEventListener('touchend', swipeEnd);
+carousel.addEventListener("touchstart", swipeStart);
+carousel.addEventListener("touchend", swipeEnd);
